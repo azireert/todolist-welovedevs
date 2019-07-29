74bbc8e969300638770bd32a5af3a073fa024d5e
azireert <bpis@hotmail.fr>
2019-07-29 23:22:42 +0200

    form to add a new welovedev worker

--- src/views/List/List.js
+++ src/views/List/List.js
@@ -3,10 +3,17 @@ import './List.css';
 import ListDetail from "../ListDetail/ListDetail";
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import {Button} from "react-bootstrap";
+import {Form} from "react-bootstrap";
 
 
 
 class List extends React.Component{
+    state = {
+        firstname: "",
+        lastname: "",
+        job: ""
+    }
     constructor(){
         super();
         this.state = {
@@ -74,6 +81,27 @@ class List extends React.Component{
             ]
         };
         this.delete = this.delete.bind(this);
+        this.addWorker = this.addWorker.bind(this);
+    }
+
+    addWorker(e) {
+        var newWorker = {
+            id: Date.now(),
+            firstname: this.state.firstname,
+            lastname: this.state.lastname,
+            job: this.state.job
+        };
+        this.setState((prevState) => {
+            return {
+                list: prevState.list.concat(newWorker)
+            };
+        });
+
+
+        this.setState( {firstname : "", lastname: "", job: ""});
+
+
+
     }
 
     delete(id){
@@ -86,11 +114,43 @@ class List extends React.Component{
 
     render() {
         return (
-            <Row>
-                <Col></Col>
-                <Col><ListDetail delete={this.delete} workers={this.state.list}/></Col>
-                <Col></Col>
-            </Row>
+            <div>
+                <Row>
+                    <Col></Col>
+                    <Col>
+                        <Form>
+                            <Form.Group controlId="formGroupFN" ref="myFrom">
+                                <Form.Label>Prénom</Form.Label>
+                                <Form.Control type="text" placeholder="Entrez votre prénom"
+                                              value={this.state.firstname}
+                                              onChange={e => this.setState({firstname: e.target.value})}
+                                />
+                            </Form.Group>
+                            <Form.Group controlId="formGroupLN">
+                                <Form.Label>Nom</Form.Label>
+                                <Form.Control type="text" placeholder="Entrez votre nom"
+                                              value={this.state.lastname}
+                                              onChange={e => this.setState({lastname: e.target.value})}/>
+                            </Form.Group>
+                            <Form.Group controlId="formGroupJob">
+                                <Form.Label>Job</Form.Label>
+                                <Form.Control type="text" placeholder="Quel est votre job ?"
+                                              value={this.state.job}
+                                              onChange={e => this.setState({job: e.target.value})}/>
+                            </Form.Group>
+                            <Form.Group>
+                                <Button onClick={this.addWorker} variant="primary">Ajouter</Button>
+                            </Form.Group>
+                        </Form>
+                    </Col>
+                    <Col></Col>
+                </Row>
+                <Row>
+                    <Col></Col>
+                    <Col><ListDetail delete={this.delete} workers={this.state.list}/></Col>
+                    <Col></Col>
+                </Row>
+            </div>
         );
     }
 }
